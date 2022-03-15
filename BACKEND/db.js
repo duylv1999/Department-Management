4c6bd5a86065284bba86919c92c2733c2d9b0efb
Duy <duysiri1999@gmail.com>
2022-03-15 14:02:06 +0700

    deploy

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'duy123',
-    host: 'localhost',
-    port: 5432,
-    database: 'quanlynhanvien'
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASS}`,
+    host: `${process.env.DB_HOST}`,
+    port: `${process.env.DB_PORT}`,
+    database: `${process.env.DB_DATABASE}`,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
